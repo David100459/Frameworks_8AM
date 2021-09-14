25309c111d12b8fc25ef21d4f9254625ddebfc4d
David100459 <davidalberto401@gmail.com>
2021-09-14 08:08:25 -0500

    Proyecto

---
+++ Pares.py
@@ -0,0 +1,17 @@
+'''
+Developer: Muñoz
+Date: 14 aug 2021
+Descripcion: Scrypt Python que permita identificar el mayor de dos
+Números ingresados por consola 
+'''
+
+import os
+
+os.system("clear")
+
+
+n1 = input("Ingrese primer numero: ")
+n2 = input("Ingrese segundo numero: ")
+
+suma = n1 + n2
+print("Resultado de la suma: ", suma)
